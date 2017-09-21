ca8c41a6e9520dd3367bceebf351d6ab3ea511b8
Adam McCurdy <amccurdy@zenoss.com>
2017-09-21 13:30:33 -0500

    Create IntrospectionRouter.py

---
+++ IntrospectionRouter.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+import sys
+from ZenAPIConnector import ZenAPIConnector
+from pprint import pprint
+
+url = 'https://YOUR_URL.zenoss.com'
+ssl_verify = True
+username = 'api_user'
+password = 'PASSWORD'
+router = 'IntrospectionRouter'
+router_endpoint = '/zport/dmd/introspection_router'
+
+def getAllRouters():
+    method = 'getAllRouters'
+    data = {}
+    api = ZenAPIConnector(url, router, router_endpoint, method, username, password, ssl_verify, data)
+    response = api.send()
+    data = response.json()['result']['data']
+    routers = {}
+    for r in data:
+        routers[r['action']] = r['urlpath']
+    return routers
+
+def getAllRouterMethods():
+    all_routers = getAllRouters()
+    method = 'getRouterMethods'
+    router_methods = {}
+    for router_name in all_routers.keys():
+        data = {'router': router_name}
+        api = ZenAPIConnector(url, router, router_endpoint, method, username, password, ssl_verify, data)
+        response = api.send()
+        sys.stdout.write('.')
+        sys.stdout.flush()
+        data = response.json()
+        router_methods[router_name] = data
+    return router_methods
+
+def printInfo(router_methods):
+    for k,v in router_methods.iteritems():
+        methods = v['result']['data']
+        for method, info in methods.iteritems():
+            print 'ROUTER NAME: %s' % (k)
+            print 'METHOD: %s' % (method)
+            print 'METHOD DOCUMENTATION: %s' % info['documentation']
+            print 'METHOD ARGS: %s' % info['args']
+            print 'METHOD KWARGS: %s' % info['kwargs']
+            print '---------------------------------------'
+        print '======================================'
+
+if __name__ == '__main__':
+    router_methods = getAllRouterMethods()
+    printInfo(router_methods)
