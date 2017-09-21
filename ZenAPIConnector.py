b711cc48390beab9874014b047388916d5d6cb2d
Adam McCurdy <amccurdy@zenoss.com>
2017-09-21 15:00:38 -0500

    Update ZenAPIConnector.py

--- ZenAPIConnector.py
+++ ZenAPIConnector.py
@@ -1,35 +1,13 @@
 #!/usr/bin/env python
 import requests
-from json import loads, dumps
-from pprint import pprint
+from json import dumps
 from ConfigParser import RawConfigParser
 
-class ZenAPIConnector():
-    def __init__(self, url, router, router_endpoint, method, username, password, ssl_verify, data):
-        self.url = url
-        self.router = router
-        self.router_endpoint = router_endpoint
-        self.api_endpoint = url + router_endpoint
-        self.method = method
-        self.username = username
-        self.password = password
-        self.headers = {'Content-Type': 'application/json'}
-        self.ssl_verify = ssl_verify
-        self.data = data
-        self.payload = dumps({'action': self.router, 'method': self.method, 'data': [data], 'tid': 1})
-
-    def send(self):
-        response = requests.post(self.api_endpoint, self.payload, headers=self.headers,
-                                 auth=(self.username, self.password), verify=self.ssl_verify)
-        if response.status_code == 200:
-            return response
-        else:
-            print 'HTTP Status: %s' % (response.status_code)
 
 class ZenAPIConfig():
-    def __init__(self, config_file):
+    def __init__(self):
         self.config = RawConfigParser()
-        self.config.read(config_file)
+        self.config.read('creds.cfg')
         self.url = self.config.get('zenoss_api', 'url')
         self.username = self.config.get('zenoss_api', 'username')
         self.password = self.config.get('zenoss_api', 'password')
@@ -47,9 +25,35 @@ class ZenAPIConfig():
     def getSSLVerify(self):
         return self.ssl_verify
 
+
+class ZenAPIConnector():
+    def __init__(self, router, router_endpoint, method, data):
+        self.config = ZenAPIConfig()
+        self.url = self.config.getUrl()
+        self.router = router
+        self.router_endpoint = router_endpoint
+        self.api_endpoint = self.url + self.router_endpoint
+        self.method = method
+        self.username = self.config.getUsername()
+        self.password = self.config.getPassword()
+        self.headers = {'Content-Type': 'application/json'}
+        self.ssl_verify = self.config.getSSLVerify()
+        self.data = data
+        self.payload = dumps({'action': self.router,
+                              'method': self.method,
+                              'data': [data],
+                              'tid': 1})
+
+    def send(self):
+        response = requests.post(self.api_endpoint,
+                                 self.payload,
+                                 headers=self.headers,
+                                 auth=(self.username, self.password),
+                                 verify=self.ssl_verify)
+        if response.status_code == 200:
+            return response
+        else:
+            print 'HTTP Status: %s' % (response.status_code)
+
 if __name__ == '__main__':
-#    api = ZenAPIConnector(url, router, router_endpoint, method, username, password, ssl_verify, data)
-#    resp = api.send()
-#    print resp.json()
-#    pprint(api.getAllRouters())
     pass
