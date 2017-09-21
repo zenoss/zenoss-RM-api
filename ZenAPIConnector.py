d8d6fff24affac9be393a94a9e0d97f089996457
Adam McCurdy <amccurdy@zenoss.com>
2017-09-21 14:39:59 -0500

    Update ZenAPIConnector.py

--- ZenAPIConnector.py
+++ ZenAPIConnector.py
@@ -2,6 +2,7 @@
 import requests
 from json import loads, dumps
 from pprint import pprint
+from ConfigParser import RawConfigParser
 
 class ZenAPIConnector():
     def __init__(self, url, router, router_endpoint, method, username, password, ssl_verify, data):
@@ -24,6 +25,27 @@ class ZenAPIConnector():
             return response
         else:
             print 'HTTP Status: %s' % (response.status_code)
+
+class ZenAPIConfig():
+    def __init__(self, config_file):
+        self.config = RawConfigParser()
+        self.config.read(config_file)
+        self.url = self.config.get('zenoss_api', 'url')
+        self.username = self.config.get('zenoss_api', 'username')
+        self.password = self.config.get('zenoss_api', 'password')
+        self.ssl_verify = bool(self.config.get('zenoss_api', 'ssl_verify'))
+
+    def getUrl(self):
+        return self.url
+
+    def getUsername(self):
+        return self.username
+
+    def getPassword(self):
+        return self.password
+
+    def getSSLVerify(self):
+        return self.ssl_verify
 
 if __name__ == '__main__':
 #    api = ZenAPIConnector(url, router, router_endpoint, method, username, password, ssl_verify, data)
