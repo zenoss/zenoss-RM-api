5af2917b9d2c27e8dc4332d49dff11936f78b7d6
Adam McCurdy <amccurdy@zenoss.com>
2017-09-25 18:23:02 -0300

    moving examples

---
+++ examples/DashboardRouter_getDeviceIssues.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+#####################################################
+# This script is a basic example of how to create   #
+# display dashboard information using the Zenoss    #
+# JSON API and the ZenAPIConnector class written by #
+# Adam McCurdy @ Zenoss                             #
+#####################################################
+
+import sys
+from ZenAPIConnector import ZenAPIConnector
+
+router = 'DashboardRouter'
+method = 'getDeviceIssues'
+data = {}
+
+def getDeviceIssues():
+    '''
+    This method makes the API call and returns the response
+    '''
+    api = ZenAPIConnector(router, method, data)
+    response = api.send()
+    resp_data = response.json()['result']
+    return resp_data
+
+def deviceIssuesReport():
+    '''
+    This method sorts through the data and prints it out in 
+    .csv format. There are several other fields one might be
+    interested in here, but here are a few as an example.
+    '''
+    report_data = getDeviceIssues()['data']
+    print 'Device, Device Class, ProdState, Clear, Debug, Info '\
+          'Warning, Error, Critical'
+    for entry in report_data:
+        device = entry['device']
+        deviceClass = entry['deviceClass']['uid'].replace('/zport/dmd/Devices', '')
+        prodState = entry['productionStateLabel']
+        events = entry['events']
+        clear = events['clear']['count']
+        debug = events['debug']['count']
+        info = events['info']['count']
+        warning = events['warning']['count']
+        error = events['error']['count']
+        critical = events['critical']['count']
+        print '%s, %s, %s, %s, %s, %s, %s, %s, %s' % (device, 
+                                                      deviceClass,
+                                                      prodState, 
+                                                      clear, 
+                                                      debug, 
+                                                      info, 
+                                                      warning, 
+                                                      error, 
+                                                      critical)
+
+if __name__ == '__main__':
+    '''
+    Run the report
+    '''
+    deviceIssuesReport()
---
+++ examples/DeviceRouter_getDevices.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+#####################################################
+# This script is a basic example of how to create a #
+# list of devices and display device-related info   #
+# using the Zenoss JSON API and the ZenAPIConnector #
+# class written by Adam McCurdy @ Zenoss            #
+#####################################################
+
+import sys
+from ZenAPIConnector import ZenAPIConnector
+
+router = 'DeviceRouter'
+method = 'getDevices'
+data = {'limit': 200}
+
+def getDevices():
+    '''
+    This makes the API call and returns data
+    '''
+    api = ZenAPIConnector(router, method, data)
+    response = api.send()
+    resp_data = response.json()['result']
+    return resp_data
+
+
+def deviceReport():
+    ''''
+    This sorts through the data and displays results in a 
+    .csv format. There are numerous other fields that can 
+    be displayed here, but here are a few as an example.
+    '''
+    device_resp = getDevices()
+    devices = device_resp['devices']
+    print 'Name, IP Address, UID, ProdState, Collector, Location'
+    for dev in devices:
+        try: 
+            location = dev['location']['uid']
+        except (KeyError, TypeError): 
+            location = ''
+        print '%s, %s, %s, %s, %s, %s' % (dev['name'], 
+                                          dev['ipAddressString'],
+                                          dev['uid'],
+                                          dev['productionState'],
+                                          dev['collector'],
+                                          location)
+
+
+if __name__ == '__main__':
+    deviceReport()
