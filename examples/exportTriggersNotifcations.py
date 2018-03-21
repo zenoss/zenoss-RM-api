9b444286a65fe62817b5a2737b830bfc846ef0c5
Steve Aiello <saiello@zenoss.com>
2018-03-21 15:59:10 -0500

    SVC-1519: Format updated to conform to pep8/pyflakes

--- examples/exportTriggersNotifcations.py
+++ examples/exportTriggersNotifcations.py
@@ -9,14 +9,16 @@ from ZenAPIConnector import ZenAPIConnector
 
 zenInstance = 'unknown'
 
+
 def TriggerRouter(sMethod, dData={}):
     global zenInstance
     sRouter = 'TriggersRouter'
     api = ZenAPIConnector(sRouter, sMethod, dData)
     response = api.send()
     zenInstance = urlparse(api.url).hostname
-    if response == None:
-        print "Error"  #todo
+    if response is None:
+        # Todo: add error handling
+        print "Error"
         exit(1)
     respData = response.json()
     if not respData['result']['success']:
@@ -27,14 +29,24 @@ def TriggerRouter(sMethod, dData={}):
         exit(1)
     return respData['result']['data']
 
+
 def write2File(sType, lData):
-    exportPath='./export_%s' % zenInstance
+    exportPath = './export_%s' % zenInstance
     if not os.path.exists(exportPath):
         os.makedirs(exportPath)
     for dEntry in lData:
-        print "INFO: writing %s/%s.%s.json" % (exportPath, dEntry['name'], sType)
+        print "INFO: writing %s/%s.%s.json" % (
+            exportPath,
+            dEntry['name'],
+            sType
+        )
         try:
-            trigFile=open("%s/%s.%s.json" % (exportPath, dEntry['name'], sType),'w')
+            trigFile = open("%s/%s.%s.json" % (
+                exportPath,
+                dEntry['name'],
+                sType),
+                'w'
+            )
             trigFile.write(json.dumps(dEntry))
             trigFile.close()
         except Exception, e:
@@ -43,19 +55,24 @@ def write2File(sType, lData):
 
 if __name__ == "__main__":
     print "Script started, exporting from %s" % zenInstance
-    lTriggers=TriggerRouter('getTriggers')
-    print "INFO: got event triggers, total of %s" % str(len(lTriggers))
+    lTriggers = TriggerRouter('getTriggers')
+    print "INFO: got event triggers, total of %s" % (
+        str(len(lTriggers))
+    )
     write2File('trigger', lTriggers)
     #
-    lNotifications=TriggerRouter('getNotifications')
-    print "INFO: got event notifications, total of %s" % str(len(lNotifications))
-    write2File('notification', lNotifications) 
+    lNotifications = TriggerRouter('getNotifications')
+    print "INFO: got event notifications, total of %s" % (
+        str(len(lNotifications))
+    )
+    write2File('notification', lNotifications)
     #
-    lNotifWindows=[]
+    lNotifWindows = []
     for dNotif in lNotifications:
-        lWindows=TriggerRouter('getWindows', dData={'uid': dNotif['uid']})
-        lNotifWindows=lNotifWindows + lWindows
-    print "INFO: got event notifications windows, total of %s" % str(len(lNotifWindows))
-    write2File('window', lNotifWindows) 
+        lWindows = TriggerRouter('getWindows', dData={'uid': dNotif['uid']})
+        lNotifWindows = lNotifWindows + lWindows
+    print "INFO: got event notifications windows, total of %s" % (
+        str(len(lNotifWindows))
+    )
+    write2File('window', lNotifWindows)
     print "Script completed"
-    
