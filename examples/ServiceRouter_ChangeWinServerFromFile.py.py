ab43cae509b276cf84b27b312ab6e08858f4e315
Steve Anness <sanness@zenoss.com>
2018-06-27 15:52:56 -0500

    Create ServiceRouter_ChangeWinServerFromFile.py.py
    Example file of processing a list of windows services and changing attributes

---
+++ examples/ServiceRouter_ChangeWinServerFromFile.py.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+#####################################################
+# This script is a basic example of how to add an   #
+# OS Process Definition into Zenoss Resource Manager#
+# using the Zenoss JSON API and ZenAPILib           #
+# in this example we use a text file where each     #
+# line contains a new process.  This was done so    #
+# the script could be re-used without modifying it  #
+#####################################################
+
+# stdlib Imports
+import json
+from sys import exit
+import os
+from urlparse import urlparse
+import zenApiLib
+
+SERVICE_ROOT= '/zport/dmd/Services/WinService/serviceclasses'
+
+def setWinServiceMonitored():
+    '''
+    This makes the API call and returns data
+    '''
+    serviceRouter = zenApiLib.zenConnector(routerName='ServiceRouter')
+    file = open("servicelist.txt", "r")
+    for service in file:
+        serviceRouter.callMethod('setInfo', uid=(SERVICE_ROOT + '/' + service.strip()), zMonitor={"isAcquired": False, "localValue": True}, monitoredStartModes='Manual')
+
+if __name__ == '__main__':
+    setWinServiceMonitored()
+   
