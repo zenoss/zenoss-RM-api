fc292e40a60350da6df8c1bb0312f52ddc9ddbc6
Steve Aiello <saiello@zenoss.com>
2018-07-02 12:46:43 -0500

    Functionality/Usage update

--- zenApiLib.py
+++ zenApiLib.py
@@ -111,8 +111,12 @@ class zenConnector():
         '''
         Call API directly and returns results directly. For paging API calls
         use self.pagingMethodCall.
+        NOTE: There are two methods to pass the method parameters:
+          preferred: self.callMethod('getDevices', uid="/zport/dmd/Devices", keys=["name","ipAddress",])
+           override: self.callMethod('getDevices', override={"data": {"uid":"/zport/dmd/Devices","keys":["name","ipAddress",]})
+        Some router methods pass data in a 'non-standard' way. Using the override method helps deal with that.
         '''
-        self.log.info('callMethod; method:%s, payload:%s' % (method, payload))
+        self.log.info('callMethod; method:%s, payload:%s' % (method, payload))                                                                              
         # Check that specified method is valid, skip 'IntrospectionRouter' router methods
         if self._routerName != 'IntrospectionRouter':
             if not (method[0] in self._routersInfo[self._routerName]['methods'].keys()):
@@ -132,8 +136,12 @@ class zenConnector():
             'method': method[0],
             'tid': self._tid,
         }
-        apiBody['data'] = [payload]
-        #print "Manual DEBUG: %s" % apiBody
+        if len(payload) == 1 and "override" in payload:
+            #override method
+            apiBody.update(payload['override'])
+        else:
+            #preferred method
+            apiBody['data'] = [payload]
         try:
             r = self.requestSession.post(self._url,
                 auth=(self.config['username'], self.config['password']),
@@ -330,4 +338,76 @@ class TitleParser(HTMLParser):
         if self.match:
             self.title = data
             self.match = False
-                
+#
+#Running directly from the command-line section of code
+#
+if __name__ == '__main__':
+    import argparse
+    from pprint import pprint
+    import sys
+    class StoreDictKeyPair(argparse.Action):
+         def __call__(self, parser, namespace, values, option_string=None):
+             my_dict = getattr(namespace, self.dest, {})
+             if my_dict == None:
+                my_dict = {}
+             for kv in values.split(","):
+                 k,v = kv.split("=")
+                 if "{" in v or "[" in v:
+                    v = v.replace("'", '"')
+                    my_dict[k] = json.loads(v)
+                 else:
+                    my_dict[k] = v
+             setattr(namespace, self.dest, my_dict)
+
+    def buildArgs():
+        parser = argparse.ArgumentParser(description='Primarily a python library to be used with your python scripts. When called directly, can be used to make simple API calls.')
+        parser.add_argument('-v', dest='loglevel', action='store',
+                        type = int, default = 30,
+                        help = 'Set script logging level (DEBUG=10, INFO=20, WARN=30, *ERROR=40, CRTITICAL=50')
+        parser.add_argument('-r', dest='rName', action='store', metavar="routerName",
+                        required=True,
+                        help = 'API router name')
+        parser.add_argument('-m', dest='rMethod', action='store', metavar="routerMethodName",
+                        required=True,
+                        help = 'API router method to use')
+        parser.add_argument('-c', dest='configSection', action='store', metavar="credsSection",
+                        default = 'default',
+                        help = 'zenApiLib credential configuration section (default)')
+        parser.add_argument('-p', dest='configFilePath', action='store', metavar="credsFilePath",
+                        default = '',
+                        help = 'Default location being the same directory as the zenApiLib.py file')
+        parser.add_argument('-d', dest='data', action=StoreDictKeyPair, metavar="KEY1=VAL1,KEY2=VAL2...",
+                        help = "Parameters to pass to router's method function")
+        parser.add_argument('-x', dest='rValue', action='store', metavar="fieldName.fieldName...",
+                        default = 'result.success', help = "Return value from API result field. Default: 'result.success'")
+        return parser.parse_args()
+
+    def nested_get(input_dict, nested_key):
+        internal_dict_value = input_dict
+        for k in nested_key:
+            if k.isdigit():
+                internal_dict_value = internal_dict_value[int(k)]
+            else:
+                internal_dict_value = internal_dict_value.get(k, None)
+            if internal_dict_value is None:
+                return None
+        return internal_dict_value    
+
+    args = vars(buildArgs())
+    log = logging.getLogger('zenApiLib')
+    log.setLevel(args['loglevel'])
+    zenapi  = zenConnector(routerName=args['rName'], cfgFilePath=args['configFilePath'],section = args['configSection'], loglevel = args['loglevel'])
+    apiResult = zenapi.callMethod(args['rMethod'], **args['data'])
+    if not nested_get(apiResult, ['result', 'success']):
+        pprint(apiResult)
+        sys.exit(1)
+    else:
+        if args['rValue'].lower() == 'all':
+            pprint(apiResult)
+        else:
+            rValue = nested_get(apiResult, args['rValue'].split('.'))
+            if rValue:
+                print rValue
+            else:
+                print "'%s' does not exist in API results:" 
+                pprint(apiResult)
