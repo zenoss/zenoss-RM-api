42916b86c571877059473f24825513a7e598a845
Steve Aiello <saiello@zenoss.com>
2018-11-01 07:37:13 -0500

    Updated zenApiLib call, no longer using deprecated class

--- zenJobsRouterHelper.py
+++ zenJobsRouterHelper.py
@@ -1,5 +1,6 @@
 import zenApiLib
 import logging
+from time import time, sleep
 
 if not ('logging' in dir()):
     import logging
@@ -8,46 +9,39 @@ if not ('logging' in dir()):
     )
     logging.getLogger().setLevel(logging.ERROR)
 
+api = zenApiLib.zenConnector(section = 'default', routerName = 'JobsRouter')
 
-class zenApiJobsRouterHelper():
-    def __init__(self, debug=False):
-        """
-        Initialize the API connection, log in, and store authentication cookie
-        """
-        self.api = zenApiLib.zenConnector(section = 'default', routerName = 'JobsRouter')
-        self.log = logging.getLogger(self.__class__.__name__)
+def checkJobStatus(jobid):
+    result = api.callMethod('getInfo', jobid=jobid)
+    try:
+        status = result['result']['data']['status']
+    except (KeyError, ValueError):
+        status = 'UNKNOWN! Invalid Job ID or other failure'
+    return status
 
-    def checkJobStatus(self, jobid):
-        result = self.api.callMethod('getInfo', jobid=jobid)
-        try:
-            status = result.json()['result']['data']['status']
-        except (KeyError, ValueError):
-            status = 'UNKNOWN! Invalid Job ID or other failure'
-        return status
-
-    def watchStatus(self, jobid, timeout=300):
-        '''
-        This is a blocking method that will check on the status of
-        a job every n seconds until it's either completed, aborted,
-        failed, or a timeout is reached. It returns a tuple with the
-        success status of the job, and a bool for Success or Failure
-        '''
-        jobStatus = 'Unknown'
-        starttime = time()
-        while jobStatus != 'SUCCESS':
-            currtime = time()
-            jobStatus = self.checkJobStatus(jobid)
-            if starttime + timeout <= currtime:
-                jobStatus = 'TIMEOUT'
-                break
-            elif jobStatus == 'ABORTED':
-                break
-            sleep(3)
-        if jobStatus == 'FAILURE':
-            return jobStatus, False
+def watchStatus(jobid, timeout=300):
+    '''
+    This is a blocking method that will check on the status of
+    a job every n seconds until it's either completed, aborted,
+    failed, or a timeout is reached. It returns a tuple with the
+    success status of the job, and a bool for Success or Failure
+    '''
+    jobStatus = 'Unknown'
+    starttime = time()
+    while jobStatus != 'SUCCESS':
+        currtime = time()
+        jobStatus = checkJobStatus(jobid)
+        if starttime + timeout <= currtime:
+            jobStatus = 'TIMEOUT'
+            break
         elif jobStatus == 'ABORTED':
-            return jobStatus, False
-        elif jobStatus == 'TIMEOUT':
-            return jobStatus, False
-        elif jobStatus == 'SUCCESS':
-            return jobStatus, True
+            break
+        sleep(3)
+    if jobStatus == 'FAILURE':
+        return jobStatus, False
+    elif jobStatus == 'ABORTED':
+        return jobStatus, False
+    elif jobStatus == 'TIMEOUT':
+        return jobStatus, False
+    elif jobStatus == 'SUCCESS':
+        return jobStatus, True
