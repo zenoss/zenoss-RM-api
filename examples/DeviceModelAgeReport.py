71798a131ff25ef80f1e755d20568afea7af19e4
Steve Aiello <saiello@zenoss.com>
2018-11-02 13:32:22 -0500

    standardizing command flags

--- examples/DeviceModelAgeReport.py
+++ examples/DeviceModelAgeReport.py
@@ -3,16 +3,43 @@ import zenApiLib
 import argparse
 import sys
 import time
-from pprint import pprint
+import logging
+from pprint import pformat
 
 def buildArgs():
     parser = argparse.ArgumentParser(description='Create a comma delimited report'
                         ' of device model status with other related data.') 
+    parser.add_argument('-v', dest='loglevel', action='store', type=int,
+                        default=30, help='Set script logging level (DEBUG=10,'
+                        ' INFO=20, WARN=30, *ERROR=40, CRTITICAL=50')
+    parser.add_argument('-p', dest='configFilePath', action='store',
+                        metavar="credsFilePath", default='', help='Default '
+                        'location being the same directory as the zenApiLib.py'
+                        'file')
+    parser.add_argument('-c', dest='configSection', action='store',
+                        metavar="credsSection", default='default',
+                        help='zenApiLib credential configuration section '
+                        '(default)')
+    parser.add_argument('-o', dest='outFileName', action='store', default=None,
+                        help="Output to file instead of stdout.")
     return parser.parse_args()
 
 if __name__ == '__main__':
+    logging.basicConfig(
+        format='%(asctime)s %(levelname)s %(name)s: %(message)s'
+    )
+    logging.getLogger().setLevel(logging.ERROR)
     args = vars(buildArgs())
-    api = zenApiLib.zenConnector()
+    log = logging.getLogger(__file__)
+    log.setLevel(args['loglevel'])
+    if args['outFileName']:
+        rOut = open(args['outFileName'], 'w')
+    else:
+        rOut = sys.stdout
+    api = zenApiLib.zenConnector(
+                        cfgFilePath=args['configFilePath'],
+                        section=args['configSection'],
+                        loglevel=args['loglevel'])
     # Get active zenmodeler events
     issues = {}
     evtQryParams = {
@@ -28,13 +55,13 @@ if __name__ == '__main__':
             if not pagedResults['result']['success']:
                 raise Exception(pagedResults['msg'])
         except Exception as e:
-            print "ERROR: {!r}".format(e)
-            pprint(pagedResults)
+            print >> sys.stderr, "ERROR: {!r}".format(e)
+            print >> sys.stderr, pformat(pagedResults)
             sys.exit(1)
         for event in pagedResults['result']['events']:
             issues[event['device']['text']] = event['summary']
     # Loop through Devices and report on model dates
-    print "Device Class, Device ID, Device Title, Last Modeled DateTime, Last Modeled Date, Production State, Model Events, Status Up/Down"
+    print >> rOut, "Device Class, Device ID, Device Title, Last Modeled DateTime, Last Modeled Date, Production State, Model Events, Status Up/Down"
     api.setRouter('DeviceRouter')
     for pagedResults in api.pagingMethodCall('getDevices',
                                              keys=['uid', 'id', 'name', 'status', 'lastCollected', 'productionStateLabel'],
@@ -43,11 +70,11 @@ if __name__ == '__main__':
             if not pagedResults['result']['success']:
                 raise Exception(pagedResults['msg'])
         except Exception as e:
-            print "ERROR: {!r}".format(e)
-            pprint(pagedResults)
+            print >> sys.stderr, "ERROR: {!r}".format(e)
+            print >> sys.stderr, pformat(pagedResults)
             sys.exit(1)
         for device in pagedResults['result']['devices']:
-            print '{},{},{},{},{},{},"{}",{}'.format(
+            print >> rOut, '{},{},{},{},{},{},"{}",{}'.format(
                 '/'.join(device['uid'].split('/')[4:-2]),
                 device['id'],
                 (device['name'] if device['name'] != device['id'] else ''),
