af1ac9b07db5d58246fe3a56ec418da2d02600fc
Adam McCurdy <amccurdy@zenoss.com>
2019-03-21 12:58:16 +0530

    fixing syntax issues

--- examples/DeviceManagementRouter_addMaintWindow_SysArgv.py
+++ examples/DeviceManagementRouter_addMaintWindow_SysArgv.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 import zenApiLib
 import time
 import sys
@@ -7,7 +8,7 @@ usage = '%s <uid> <durationHours> <prodState> <name>' % (sys.argv[0])
 if len(sys.argv) != 5:
     print usage
     sys.exit(1)
-else: 
+else:
     uid = sys.argv[1]
     durationHours = sys.argv[2]
     prodState = sys.argv[3]
@@ -15,17 +16,19 @@ else:
 
 now = int(time.time())
 
-dmr = zenApiLib.setConnector(routerName='DeviceManagementRouter')
+dmr = zenApiLib.zenConnector()
 
-params = {"uid": uid, 
-          "id": "", 
-          "name": name, 
-          "startDateTime": now, 
-          "durationDays":"00", 
-          "durationHours": durationHours, 
-          "durationMinutes":"00", 
-          "repeat":"Never", 
-          "startProductionState": prodState, 
+dmr.setRouter(routerName='DeviceManagementRouter')
+
+params = {"uid": uid,
+          "id": "",
+          "name": name,
+          "startDateTime": now,
+          "durationDays":"00",
+          "durationHours": durationHours,
+          "durationMinutes":"00",
+          "repeat":"Never",
+          "startProductionState": prodState,
           "enabled": True
           }
 
