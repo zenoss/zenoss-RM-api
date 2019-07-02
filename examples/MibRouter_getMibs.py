601fabf1ba6e38d05b3f176885011f3e5c727b01
Bryan Irvine <sparctacus@gmail.com>
2019-07-02 17:10:00 -0500

    Create example for mibs router

---
+++ examples/MibRouter_getMibs.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+#####################################################
+# This script is a basic example of how to create a #
+# list of mibs and display mib-related info   #
+# using the Zenoss JSON API and the ZenAPIConnector #
+# class written by Adam McCurdy @ Zenoss            #
+#####################################################
+
+from ZenAPIConnector import ZenAPIConnector
+
+router = 'MibRouter'
+method = 'getOidMappings'
+data = {"uid":"/zport/dmd/Mibs/mibs/BRIDGE-MIB"}
+
+
+def getMibs():
+    '''
+    This makes the API call and returns data
+    '''
+    api = ZenAPIConnector(router, method, data)
+    response = api.send()
+    resp_data = response.json()['result']
+    return resp_data
+
+
+def mibReport():
+    ''''
+    This sorts through the data and displays results in a
+    .csv format. There are numerous other fields that can
+    be displayed here, but here are a few as an example.
+    '''
+    mib_resp = getMibs()
+    mibs = mib_resp['data']
+    print 'oid, UID'
+    for mib in mibs:
+        print '%s, %s' % (mib['oid'],
+                               mib['uid'])
+
+if __name__ == '__main__':
+    mibReport()
